3198cffd5bd9b804c0e9ef62e307117501b82420
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-07-05 18:52:55 -0300

    ref: refatoração da sintaxe de declaração do estilo dos ElevatedButton

--- lib/view/components/button_pick_image.dart
+++ lib/view/components/button_pick_image.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class ButtonPickImage extends StatefulWidget {
   final String text;
   final VoidCallback onButtonPresed;
-  const ButtonPickImage({super.key, required this.text, required this.onButtonPresed});
+  const ButtonPickImage(
+      {super.key, required this.text, required this.onButtonPresed});
 
   @override
   State<ButtonPickImage> createState() => _ButtonPickImage();
@@ -13,21 +14,17 @@ class _ButtonPickImage extends State<ButtonPickImage> {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: widget.onButtonPresed,
-      style: ButtonStyle(
-        backgroundColor: WidgetStatePropertyAll(
-          Color.fromRGBO(53, 88, 52, 1)
-        ),
-        padding: WidgetStatePropertyAll(
-          const EdgeInsets.fromLTRB(60, 15, 60, 15)
-        ),
-        shape: WidgetStatePropertyAll(
-          RoundedRectangleBorder(
+        onPressed: widget.onButtonPresed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromRGBO(53, 88, 52, 1),
+          padding: const EdgeInsets.fromLTRB(60, 15, 60, 15),
+          shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.0),
           ),
         ),
-      ),
-      child: Text(widget.text, style: TextStyle(color: Colors.white),)
-    );
+        child: Text(
+          widget.text,
+          style: TextStyle(color: Colors.white),
+        ));
   }
 }
--- lib/view/components/button_recognize_vehicle.dart
+++ lib/view/components/button_recognize_vehicle.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class ButtonRecognizeVehicle extends StatefulWidget {
   final String text;
   final VoidCallback onButtonPresed;
-  const ButtonRecognizeVehicle({super.key, required this.text, required this.onButtonPresed});
+  const ButtonRecognizeVehicle(
+      {super.key, required this.text, required this.onButtonPresed});
 
   @override
   State<ButtonRecognizeVehicle> createState() => _ButtonRecognizeVehicle();
@@ -14,14 +15,11 @@ class _ButtonRecognizeVehicle extends State<ButtonRecognizeVehicle> {
   Widget build(BuildContext context) {
     return ElevatedButton(
         onPressed: widget.onButtonPresed,
-        style: ButtonStyle(
-          backgroundColor:
-              WidgetStatePropertyAll(Color.fromRGBO(53, 88, 52, 1)),
-          minimumSize: WidgetStatePropertyAll(Size(double.infinity, 56)),
-          shape: WidgetStatePropertyAll(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromRGBO(53, 88, 52, 1),
+          minimumSize: Size(double.infinity, 56),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
           ),
         ),
         child: Text(widget.text, style: TextStyle(color: Colors.white)));
