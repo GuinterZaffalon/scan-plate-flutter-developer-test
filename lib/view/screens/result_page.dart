a16623ec1cd3e8f8dbe057d9a61976f0095e2d21
Guinter Zaffalon <143457091+GuinterZaffalon@users.noreply.github.com>
2025-07-06 11:51:40 -0300

    feat: tratamento de erro e exibição dos dados na view com FutureBuilder

--- lib/view/screens/result_page.dart
+++ lib/view/screens/result_page.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:scan_plate/view_model/plate_result_format.dart';
+import 'package:scan_plate/view_model/search_car.dart';
 
 class ResultPage extends StatefulWidget {
   final File file;
@@ -12,6 +14,13 @@ class ResultPage extends StatefulWidget {
 }
 
 class _ResultPageState extends State<ResultPage> {
+  late Future<List<PlateResult>> futureResults;
+
+  @override
+  void initState() {
+    super.initState();
+    futureResults = SearchCar.searchCarByPlateRecognizer(widget.file);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,12 +36,47 @@ class _ResultPageState extends State<ResultPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(9),
-        child: Column(
-          children: [
-            
-          ],
-        ),
-      ),
-    );
+        child: FutureBuilder<List<PlateResult>>(
+          future: futureResults, 
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator(color: Color.fromRGBO(53, 88, 52, 1)));
+            } else if (snapshot.hasError) {
+              return Column(
+                children: [ 
+                  Text("Ocorreu um erro de conexão, tente novamente!",
+                  style: GoogleFonts.roboto(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18))
+                ]
+              );
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Column(
+                children: [ 
+                  Text("Não foi possível reconhecer o veículo \n tente novamente!",
+                  style: GoogleFonts.roboto(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18))
+                ]
+              );
+            } 
+            final results = snapshot.data!;
+            return ListView.builder(itemBuilder: (context, index){
+              return Card(
+                child: ListTile(
+                  title: Text(
+                    'Modelo e Marca: ${results[index].modelMake.isNotEmpty ? '${results[index].modelMake[0].make} ${results[index].modelMake[0].model}' : 'Desconhecido'}',
+                  ),
+                  subtitle: Text(
+                    'Cor: ${results[index].color.isNotEmpty ? results[index].color : 'Desconhecida'}'
+                  ),
+                ),
+              );
+            });
+          })
+        )
+      );
   }
 }
